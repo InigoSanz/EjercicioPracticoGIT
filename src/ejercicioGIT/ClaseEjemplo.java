7109361e2fd9c9a4c24fe4bc066eecc4f2b917ff
InigoSanz <i.sanzdelg@gmail.com>
2025-02-12 16:57:24 +0100

    Commit

--- src/ejercicioGIT/ClaseEjemplo.java
+++ src/ejercicioGIT/ClaseEjemplo.java
@@ -5,11 +5,7 @@ public class ClaseEjemplo {
 	public static void main(String[] args) {
 		
 		System.out.println("hola mundo");
-<<<<<<< HEAD
 		System.out.println("hola mundo de nuevo en master");
-=======
-		System.out.println("hola mundo de nuevo en rama");
->>>>>>> refs/heads/nuevaRama
 		
 	}
 }
